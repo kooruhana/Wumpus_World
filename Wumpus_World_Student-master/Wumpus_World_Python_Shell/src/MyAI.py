24f33ce6d010e7f40d93e64f1f67668c7a8ea2bb
Nelson Rong <vroy1016@gmail.com>
2018-11-17 00:35:13 -0800

    add back function, passed minor test

--- Wumpus_World_Student-master/Wumpus_World_Python_Shell/src/MyAI.py
+++ Wumpus_World_Student-master/Wumpus_World_Python_Shell/src/MyAI.py
@@ -18,6 +18,7 @@
 # ======================================================================
 
 from Agent import Agent
+from collections import deque
 
 class MyAI ( Agent ):
 
@@ -28,8 +29,9 @@ class MyAI ( Agent ):
         self.hasArrow = True
         self.grabbed = False
         self.percepts = []
-        self.explored = [(1,1)]
-        self.minimalCounter = 0
+        self.explored = deque()
+        self.makeUturn = False
+        self.uTurnCount = 0
 
 
     def getAction( self, stench, breeze, glitter, bump, scream ):
@@ -44,25 +46,28 @@ class MyAI ( Agent ):
             #print("Run away")
             return Agent.Action.CLIMB
         else:
+            if self.grabbed == True and self.currentPos == (1,1):
+                return Agent.Action.CLIMB
+            elif self.grabbed == True and self.currentPos != (1,1) and self.makeUturn == True and self.uTurnCount < 2:
+                """
+                    问题：在uturn的时候每个move都要回到这个function，第二次left的时候uturn条件已经没有了，uturn只run一次
+                """
+                self.uTurnCount+=1
+                return self.uTurn()
+            elif self.grabbed == True and self.currentPos != (1,1) and self.makeUturn == False:
+                return self.goHome()
             if stench and self.hasArrow == False:
                 "When percepts stench and you have no arrow. Escape"
                 return
             elif glitter:
                 self.grabbed = True
                 return self.foundGold()
-            if 0 < self.minimalCounter < 3:
-                """
-                Minimal AI
-                When it moves one step ahead, turn around immediately 
-                """
-                self.grabbed = True # Start going back
-                #print("going back")
-                self.minimalCounter = self.minimalCounter+1
-                return self.uTurn()
-            if self.grabbed == True and self.currentPos == (1,1):
-                return Agent.Action.CLIMB
+            elif bump:
+                # if bump, turn until no bump
+                self.explored.append(Agent.Action.TURN_LEFT)
+                return self.turnLeft()
             else:
-                self.minimalCounter = self.minimalCounter + 1
+                self.explored.append(Agent.Action.FORWARD)
                 return self.takeMove()
 
     def takeMove(self):
@@ -91,11 +96,53 @@ class MyAI ( Agent ):
         for a in actionL:
             return a
 
+    def turnLeft(self):
+        if self.direction == 'right':
+            self.direction = 'up'
+        elif self.direction == 'left':
+            self.direction = 'down'
+        elif self.direction == 'up':
+            self.direction = 'left'
+        elif self.direction == 'down':
+            self.direction = 'right'
+        return Agent.Action.TURN_LEFT
+
+    def turnRight(self):
+        if self.direction == 'right':
+            self.direction = 'down'
+        elif self.direction == 'left':
+            self.direction = 'up'
+        elif self.direction == 'up':
+            self.direction = 'right'
+        elif self.direction == 'down':
+            self.direction = 'left'
+        return Agent.Action.TURN_RIGHT
     def shotArrow(self):
         self.hasArrow = False
         return Agent.Action.SHOOT
 
     def foundGold(self):
         self.grabbed = True
+        self.makeUturn = True
         return Agent.Action.GRAB
 
+    def goHome(self):
+        self.uTurn()
+        for move in range(len(self.explored)):
+            nextMove = self.explored.pop()
+            if nextMove == Agent.Action.TURN_LEFT:
+                nextMove = Agent.Action.TURN_RIGHT
+            elif nextMove == Agent.Action.TURN_RIGHT:
+                nextMove = Agent.Action.TURN_LEFT
+            return nextMove
+
+
+            # if 0 < self.minimalCounter < 3:
+            #     """
+            #     Minimal AI
+            #     When it moves one step ahead, turn around immediately
+            #     """
+            #     self.grabbed = True # Start going back
+            #     #print("going back")
+            #     self.minimalCounter = self.minimalCounter+1
+            #     return self.uTurn()
