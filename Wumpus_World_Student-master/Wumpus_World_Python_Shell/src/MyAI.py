f46c4e1ffddcadb82a394fc22d6a793082c52422
Nelson Rong <vroy1016@gmail.com>
2018-11-17 21:08:12 -0800

    用hardcode提升分数

--- Wumpus_World_Student-master/Wumpus_World_Python_Shell/src/MyAI.py
+++ Wumpus_World_Student-master/Wumpus_World_Python_Shell/src/MyAI.py
@@ -17,6 +17,10 @@
 #                be lost when the tournament runs your code.
 # ======================================================================
 
+"""
+    一直forward 碰见gold就捡，碰见任何breeze或者stench都直接掉头
+"""
+
 from Agent import Agent
 from collections import deque
 
@@ -28,10 +32,13 @@ class MyAI ( Agent ):
         self.currentPos = (1,1)
         self.hasArrow = True
         self.grabbed = False
-        self.percepts = []
+        self.nextMove = deque()
         self.explored = deque()
         self.makeUturn = False
-        self.uTurnCount = 0
+        self.back = False
+        self.oneMore = False # hard code
+        self.uTurnCount = 0 # 因为uTurn要左转两次，用来作为判断条件（Count<2）
+        self.ForceuTurnCount = 0 # hard code
 
 
     def getAction( self, stench, breeze, glitter, bump, scream ):
@@ -46,77 +53,93 @@ class MyAI ( Agent ):
             #print("Run away")
             return Agent.Action.CLIMB
         else:
-            if self.grabbed == True and self.currentPos == (1,1):
-                return Agent.Action.CLIMB
-            elif self.grabbed == True and self.currentPos != (1,1) and self.makeUturn == True and self.uTurnCount < 2:
-                """
-                    问题：在uturn的时候每个move都要回到这个function，第二次left的时候uturn条件已经没有了，uturn只run一次
-                """
-                self.uTurnCount+=1
-                return self.uTurn()
-            elif self.grabbed == True and self.currentPos != (1,1) and self.makeUturn == False:
-                return self.goHome()
-            if stench and self.hasArrow == False:
-                "When percepts stench and you have no arrow. Escape"
-                return
-            elif glitter:
+            # print(self.currentPos)
+            if glitter:
                 self.grabbed = True
                 return self.foundGold()
-            elif bump:
+            if self.back and self.currentPos == (1,1):
+                return Agent.Action.CLIMB
+
+            if self.oneMore == True:
+                self.oneMore = False
+                self.nextMove.append(Agent.Action.TURN_LEFT)
+                return self.takeMove(self.nextMove.pop())
+
+            if self.grabbed == True and self.makeUturn == True:
+                self.uTurnCount+=1
+                self.back = True
+                if self.uTurnCount <= 2:
+                    self.explored.append(Agent.Action.TURN_LEFT)
+                return self.takeMove(self.explored.pop())
+
+            if bump and self.grabbed == False and self.makeUturn == False and self.ForceuTurnCount < 2:
                 # if bump, turn until no bump
-                self.explored.append(Agent.Action.TURN_LEFT)
-                return self.turnLeft()
+                #self.explored.append(Agent.Action.TURN_LEFT)
+                if self.ForceuTurnCount == 0:
+                    self.nextMove.extend([Agent.Action.TURN_LEFT,Agent.Action.TURN_LEFT])
+                self.ForceuTurnCount += 1
+                self.back = True
+                self.oneMore = True
+                return self.takeMove(self.nextMove.pop())
+
+            if stench and self.grabbed == False and self.makeUturn == False and self.ForceuTurnCount < 2:
+                if self.ForceuTurnCount == 0:
+                    self.nextMove.extend([Agent.Action.TURN_LEFT,Agent.Action.TURN_LEFT])
+                self.ForceuTurnCount += 1
+                self.back = True
+                return self.takeMove(self.nextMove.pop())
+
+            if breeze and self.grabbed == False and self.makeUturn == False and self.ForceuTurnCount < 2:
+                if self.ForceuTurnCount == 0:
+                    self.nextMove.extend([Agent.Action.TURN_LEFT,Agent.Action.TURN_LEFT])
+                self.ForceuTurnCount += 1
+                self.back = True
+                return self.takeMove(self.nextMove.pop())
+
+            self.explored.append(Agent.Action.FORWARD)
+            self.nextMove.append(Agent.Action.FORWARD)
+            return self.takeMove(self.nextMove.pop())
+
+    def takeMove(self,move):
+        # qprint("direction: "+self.direction)
+        if move == Agent.Action.FORWARD:
+            if self.direction == 'right':
+                self.currentPos = (self.currentPos[0]+1,self.currentPos[1])
+            elif self.direction == 'left':
+                self.currentPos = (self.currentPos[0]-1,self.currentPos[1])
+            elif self.direction == 'up':
+                self.currentPos = (self.currentPos[0],self.currentPos[1]+1)
+            elif self.direction == 'down':
+                self.currentPos = (self.currentPos[0],self.currentPos[1]-1)
+            #print(self.currentPos)
+            return Agent.Action.FORWARD
+
+        elif move in [Agent.Action.TURN_LEFT,Agent.Action.TURN_RIGHT]:
+            if move == Agent.Action.TURN_LEFT:
+                if self.direction == 'right':
+                    self.direction = 'up'
+                elif self.direction == 'left':
+                    self.direction = 'down'
+                elif self.direction == 'up':
+                    self.direction = 'left'
+                elif self.direction == 'down':
+                    self.direction = 'right'
+                return Agent.Action.TURN_LEFT
             else:
-                self.explored.append(Agent.Action.FORWARD)
-                return self.takeMove()
-
-    def takeMove(self):
-        if self.direction == 'right':
-            self.currentPos = (self.currentPos[0]+1,self.currentPos[1])
-        elif self.direction == 'left':
-            self.currentPos = (self.currentPos[0]-1,self.currentPos[1])
-        elif self.direction == 'up':
-            self.currentPos = (self.currentPos[0],self.currentPos[1]+1)
-        elif self.direction == 'down':
-            self.currentPos = (self.currentPos[0],self.currentPos[1]-1)
-        #print(self.currentPos)
-        return Agent.Action.FORWARD
+                if self.direction == 'right':
+                    self.direction = 'down'
+                elif self.direction == 'left':
+                    self.direction = 'up'
+                elif self.direction == 'up':
+                    self.direction = 'right'
+                elif self.direction == 'down':
+                    self.direction = 'left'
+                return Agent.Action.TURN_RIGHT
 
 
-    def uTurn(self):
-        actionL = [Agent.Action.TURN_LEFT,Agent.Action.TURN_LEFT]
-        if self.direction == 'right':
-            self.direction = 'up'
-        elif self.direction == 'left':
-            self.direction = 'down'
-        elif self.direction == 'up':
-            self.direction = 'left'
-        elif self.direction == 'down':
-            self.direction = 'right'
-        for a in actionL:
-            return a
+    # def uTurn(self):
+    #     actionL = [Agent.Action.TURN_LEFT,Agent.Action.TURN_LEFT]
 
-    def turnLeft(self):
-        if self.direction == 'right':
-            self.direction = 'up'
-        elif self.direction == 'left':
-            self.direction = 'down'
-        elif self.direction == 'up':
-            self.direction = 'left'
-        elif self.direction == 'down':
-            self.direction = 'right'
-        return Agent.Action.TURN_LEFT
-
-    def turnRight(self):
-        if self.direction == 'right':
-            self.direction = 'down'
-        elif self.direction == 'left':
-            self.direction = 'up'
-        elif self.direction == 'up':
-            self.direction = 'right'
-        elif self.direction == 'down':
-            self.direction = 'left'
-        return Agent.Action.TURN_RIGHT
     def shotArrow(self):
         self.hasArrow = False
         return Agent.Action.SHOOT
@@ -127,7 +150,7 @@ class MyAI ( Agent ):
         return Agent.Action.GRAB
 
     def goHome(self):
-        self.uTurn()
+        # print("going home")
         for move in range(len(self.explored)):
             nextMove = self.explored.pop()
             if nextMove == Agent.Action.TURN_LEFT:
